c14150ea11794fc8e31b0e6772c6d6fc9ddbc557
hamidteimouri <Hamid Teimouri>
2020-04-22 15:09:42 +0430

    changed

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    # After Step 1
+    $token = '68b5431d8ace4297924f69b6b570b333';
+
+    $blockCypherHelper = new \App\Libraries\BlockCypherHelper();
+    $tx = "01000000011935b41d12936df99d322ac8972b74ecff7b79408bbccaf1b2eb8015228beac8000000006b483045022100921fc36b911094280f07d8504a80fbab9b823a25f102e2bc69b14bcd369dfc7902200d07067d47f040e724b556e5bc3061af132d5a47bd96e901429d53c41e0f8cca012102152e2bb5b273561ece7bbe8b1df51a4c44f5ab0bc940c105045e2cc77e618044ffffffff0240420f00000000001976a9145fb1af31edd2aa5a2bbaa24f6043d6ec31f7e63288ac20da3c00000000001976a914efec6de6c253e657a9d5506a78ee48d89762fb3188ac00000000";
+    //$res = $blockCypherHelper->pushTransaction($tx);
+    $res = $blockCypherHelper->createNewTransaction($tx);
+//    return $res;
+
+
+});
